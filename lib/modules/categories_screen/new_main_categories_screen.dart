92ac38a42c7db1e96d6c67e98b986ccf6d5fe833
muslimadel <muslimadille@gmail.com>
2024-01-09 16:37:12 +0200

    online verions

---
+++ lib/modules/categories_screen/new_main_categories_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class NewMainCategoriesScreen extends StatefulWidget {
+  const NewMainCategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  State<NewMainCategoriesScreen> createState() => _NewMaiinCategoriesScreenState();
+}
+
+class _NewMaiinCategoriesScreenState extends State<NewMainCategoriesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      /// header
+      Row(children: [
+
+      ],)
+      /// kenow more
+      ///
+      /// categories horiizontal sliders
+      ///
+      /// home offers
+    ],);
+  }
+}
